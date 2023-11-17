be47f0f68ecbeb3744c9ba2c8cf9a684e67a93e6
Kevin <201809368@est.umss.edu>
2023-11-17 01:02:45 -0400

    First commit

---
+++ src/main/java/com/diplomado/segundotrabajo/SegundoTrabajoApplication.java
@@ -0,0 +1,13 @@
+package com.diplomado.segundotrabajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoTrabajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoTrabajoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diplomado/segundotrabajo/SegundoTrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diplomado.segundotrabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoTrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
